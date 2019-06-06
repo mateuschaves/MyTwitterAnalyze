fc0e178f70d20a601547e83669b8df428e2e3838
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-06-06 17:57:25 -0300

    aaaaaa

---
+++ fudeu.py
@@ -0,0 +1,46 @@
+from pymongo import MongoClient, ASCENDING
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+
+
+client = MongoClient(
+    'mongodb+srv://mathchaves:zoom4444@cluster0-wlooi.mongodb.net/test?retryWrites=true&w=majority')
+
+database = client['test']
+tweets = database['tweets']
+
+count = 0
+
+emotions = ["sadness", "joy", "fear", "disgust", "anger"]
+
+avarageEmotions = [0, 0, 0, 0, 0]
+
+tts = [tweet for tweet in tweets.find()]
+
+for l in range(0, 4):
+    for p, tweet in enumerate(tts):
+        # todos os tweets de um usuário
+        tweetsFromUser = [i for i in tweets.find({"author": tweet['author']})]
+        # Pra cada tweet, verificar
+        date = datetime.datetime.now()
+        time = 0
+        if(avarageEmotions[l] == 0):
+            time = date
+        else:
+            time = avarageEmotions[l]
+        for i, k in enumerate(tweetsFromUser):
+            try:
+                if k['emotion']['emotion'] == emotions[l] and k['emotion']['emotion'] != tweetsFromUser[i + 1]['emotion']['emotion']:
+                    time += (tweetsFromUser[i + 1]
+                             ['createdAt'] - k['createdAt']) / (i + 1)
+                    print(time)
+            except:
+                pass
+        if(avarageEmotions[l] == 0):
+            time = time - date
+        time = time / (p + 1)
+        print(time)
+        break
+
+    break
